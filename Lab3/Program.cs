e790cfd36107213ec0bf64e46cd53432f0a581d2
xyiakacobaka <theneetfreekgames@gmail.com>
2024-05-16 11:13:35 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Program.cs
@@ -0,0 +1,31 @@
+﻿using Lab3;
+using System;
+
+namespace FirstEF6App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33};
+                User user2 = new User { Name = "Sam", Age = 26};
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users;
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Lab3/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab3
+{
+    internal class UserContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=userstore;Trusted_Connection=True;");
+        }
+    }
+}
